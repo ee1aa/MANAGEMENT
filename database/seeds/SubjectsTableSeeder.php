2b9d39c2f4129691cab4927ba60464784259072f
kotatumikan <1471033@gmail.com>
2024-10-13 13:10:47 +0900

    あ

--- database/seeds/SubjectsTableSeeder.php
+++ database/seeds/SubjectsTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Users\Subjects;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class SubjectsTableSeeder extends Seeder
 {
@@ -13,5 +15,19 @@ class SubjectsTableSeeder extends Seeder
     public function run()
     {
         // 国語、数学、英語を追加
+        DB::table('subjects')->insert([
+            [
+                'subject' => '国語',
+                'created_at' => Carbon::now()
+            ],
+            [
+                'subject' => '数学',
+                'created_at' => Carbon::now()
+            ],
+            [
+                'subject' => '英語',
+                'created_at' => Carbon::now()
+            ]
+        ]);
     }
 }
